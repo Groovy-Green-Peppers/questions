30de1479b20253c1e4c8cc50cb4077d91a222775
Noriel <flores1noriel@gmail.com>
2023-05-25 11:51:30 -0700

    Created a SQL seed file and boiler plate code for a server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server available at http://localhost:${PORT}`);
+});
---
+++ server/schema.sql
@@ -0,0 +1,57 @@
+SELECT 'CREATE DATABASE qa' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'questions')\gexec
+-- connect to DB
+\c qa;
+
+CREATE TABLE IF NOT EXISTS questions (
+	id SERIAL PRIMARY KEY,
+  product_id INT,
+	body VARCHAR(1000),
+	date_written BIGINT,
+	asker_name VARCHAR(255),
+  asker_email VARCHAR(255),
+	reported INT,
+  helpful INT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS answers (
+  id SERIAL PRIMARY KEY,
+  question_id INT,
+  body VARCHAR(1000),
+  date_written BIGINT,
+  answerer_name VARCHAR(255),
+  answerer_email VARCHAR(255),
+  reported INT,
+  helpful INT DEFAULT 0,
+  FOREIGN KEY (question_id)
+  REFERENCES questions(id)
+);
+
+CREATE TABLE IF NOT EXISTS photos (
+  id SERIAL PRIMARY KEY,
+  answer_id INT,
+  url TEXT,
+  FOREIGN KEY (answer_id)REFERENCES answers(id)
+);
+
+-- copy CSV data over
+
+COPY questions
+FROM '/Users/noriel/Pre/SDC/questions/data/questions.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY answers
+FROM '/Users/noriel/Pre/SDC/questions/data/answers.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY photos
+FROM '/Users/noriel/Pre/SDC/questions/data/answers_photos.csv'
+DELIMITER ','
+CSV HEADER;
+-- Example copy command:
+-- COPY persons(first_name, last_name, dob, email)
+-- FROM 'C:\sampledb\persons.csv'
+-- DELIMITER ','
+-- CSV HEADER;
+-- /Users/noriel/Pre/SDC/questions/server/schema.sql
